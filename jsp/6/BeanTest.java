88fa5dc586844103d0359626acb9d541c67f8632
Jung <Jung@172.17.203.183>
2022-10-12 17:50:35 +0900

    bin file

---
+++ jsp/6/BeanTest.java
@@ -0,0 +1,16 @@
+package bean.user;
+
+public class BeanTest {
+	private String userName;
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	public String getUserName() {
+		return userName;
+	}
+	
+	
+
+}
---
+++ jsp/6/Dice.java
@@ -0,0 +1,17 @@
+package bean.user;
+
+public class Dice {
+	private int num;
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	
+	
+
+}
