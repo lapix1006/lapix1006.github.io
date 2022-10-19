37c9b7059b8e1b6d935c07b35bc7246a178108db
Jung <Jung@172.17.203.122>
2022-10-19 16:35:06 +0900

    new files

---
+++ jsp/7/Dice2.java
@@ -0,0 +1,27 @@
+package bean.user;
+
+public class Dice2 {
+	private int num;
+	private int startNum;
+	private int endNum;
+
+
+	public void setStartNum(int startNum) {
+		this.startNum = startNum;
+	}
+	public int getStartNum() {
+		return startNum;
+	}
+	public int getEndNum() {
+		return endNum;
+	}
+	public void setEndNum(int endNum) {
+		this.endNum = endNum;
+	}
+	public int getNum() {
+		num = (int)(Math.random()*(endNum-startNum)) + startNum;
+		return num;
+	}
+	
+
+}
